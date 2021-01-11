3a1da5e715b3ebd7aedb066185e340514e9eefef
TimePHPer <244605960@qq.com>
2021-01-11 23:49:36 +0800

    公益平台项目初始化

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
-module admin
+module lianquan
 
 go 1.14
+
+require (
+	github.com/HdrHistogram/hdrhistogram-go v1.0.1 // indirect
+	github.com/fsnotify/fsnotify v1.4.9
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/universal-translator v0.17.0
+	github.com/go-playground/validator/v10 v10.2.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/juju/ratelimit v1.0.1
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/spf13/viper v1.7.1
+	github.com/uber/jaeger-client-go v2.25.0+incompatible
+	github.com/uber/jaeger-lib v2.4.0+incompatible // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+)
---
+++ pkg/setting/setting.go
@@ -0,0 +1,39 @@
+package setting
+
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+type Setting struct {
+	vp *viper.Viper
+}
+
+func NewSetting(configs ...string)(*Setting,error){
+	vp := viper.New()
+	vp.SetConfigFile("config")
+	for _,config := range configs {
+		if config != ""{
+			vp.AddConfigPath(config)
+		}
+	}
+	vp.SetConfigType("yaml")
+	err := vp.ReadInConfig()
+	if err != nil {
+		return nil,err
+	}
+	s := &Setting{vp}
+	s.WatchSettingConfig()
+	return s,nil
+}
+
+func (s *Setting) WatchSettingConfig(){
+	go func() {
+		s.vp.WatchConfig()
+		s.vp.OnConfigChange(func(in fsnotify.Event) {
+			s.ReadAllSection()
+			fmt.Println("配置文件修改了")
+		})
+	}()
+}
